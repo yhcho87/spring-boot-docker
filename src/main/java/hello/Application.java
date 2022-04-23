cf630fb514b1554913b5986a0bc677bf93575764
YoungHwan <response@kakao.com>
2022-04-23 14:35:02 +0900

    change message

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Oracle Cloud World!!! - Trigger";
+        return "Hello Oracle Cloud World!!! - Blue/Green Deployment";
     }
 
 }
